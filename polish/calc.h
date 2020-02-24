fc691d68c2496ee5618e8d19c9dad7d67103dbf6
Dmitrii Zverev <zverevdmitri.87@gmail.com>
2020-02-24 15:28:31 +0300

    Add recursion.

---
+++ polish/calc.h
@@ -0,0 +1,11 @@
+#define NUMBER '0'
+
+int getop(char []);
+void push(double);
+double pop(void);
+double peek(void);
+void copy(void);
+void swipe(void);
+void clear(void);
+int getch(void);
+void ungetch(int c);
