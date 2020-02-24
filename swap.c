11c82e48224f055b44da5b5d8aed3a0e8d6b197c
Dmitrii Zverev <zverevdmitri.87@gmail.com>
2020-02-24 17:03:38 +0300

    Add swipe

---
+++ swap.c
@@ -0,0 +1,11 @@
+# include <stdio.h>
+
+// # define swap(t,x,y) {t temp = x; x = y; y = temp;}
+# define swap(t,x,y) {t temp = x; x = y; y = temp;}
+
+int main(void)
+{
+    int ai=1, bi=2;
+    swap(int, ai, bi);
+    return 0;
+}
